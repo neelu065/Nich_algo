bc838ff8761ff1a36038143b0226bbe7c7c3341d
neelu065 <52276776+neelu065@users.noreply.github.com>
2019-09-16 10:22:12 +0530

    n minimum vectors
    finding first n nearest vectors need to be corrected.

---
+++ fNCDE/main.py
@@ -0,0 +1,25 @@
+import time
+from de import de
+from func import func1
+from figure_plot import figure_plot
+import matplotlib.pyplot as plt
+
+# Inputs
+
+Fn           = 2                                                               # Function to be evaluated
+popsize      = 20
+mut          = 0.65
+crossp       = 0.65
+iter_max     = 1
+func_eval    = func1                                                           # func to be evaluated
+
+# Execution
+start = time.time()
+a , b  = de(func_eval, mut, crossp, popsize, iter_max, Fn)
+end = time.time()
+
+print('Time taken to Execute this code = {} seconds'.format(end - start))
+
+if len(a[0]) == 2:
+    plt.title('Target vector after {} iteration'.format(iter_max))             # plot for reference 
+    figure_plot(a , popsize )                                                  # comment if unnecessary
