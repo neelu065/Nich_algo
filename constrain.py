713e8a7f8d5a3c9ca25fe7f689266d85f186afb7
neelu065 <52276776+neelu065@users.noreply.github.com>
2019-09-17 22:11:49 +0530

    updated constraint

--- constrain.py
+++ constrain.py
@@ -1,7 +1,6 @@
 from constrain_const import constrain_const
 import numpy as np
 
-#
 
 def const_violation(x , Fn):
     D, P = constrain_const(Fn)                                                  # P = number of constraint
@@ -10,7 +9,11 @@ def const_violation(x , Fn):
         ap = 0
         for d in np.arange(D):
             Cp_old = np.mod(D - (p+1) + (d+1) + 1 , D)
-            Cp = np.where(Cp_old != 0 , Cp_old , D)
+            if Cp_old != 0:
+                Cp = Cp_old
+            else:
+                Cp = D
+            
             ap = ap + (Cp * x[d]) ** 2
         
         gp = D**2 - ap
@@ -18,7 +21,6 @@ def const_violation(x , Fn):
     const_viol = sum(phi_indv)                                               # constraint violation
 
     return const_viol
-
 
 # sine function constraint
 #def const_violation(a , Fn):
@@ -59,5 +61,5 @@ def const_violation(x , Fn):
 #         phi_indv = [max(0,gp[i]) for i in range(8)]
 #         const_viol = sum(phi_indv)
 #     return const_viol
-#
+##
 
