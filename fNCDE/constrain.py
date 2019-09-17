5d96d0db450bddf6d30307bb8e3857af05f4116b
neelu065 <52276776+neelu065@users.noreply.github.com>
2019-09-17 22:07:10 +0530

    Update constrain.py

--- fNCDE/constrain.py
+++ fNCDE/constrain.py
@@ -1,20 +1,20 @@
 from constrain_const import constrain_const
 import numpy as np
 
-#
-def const_violation(a , Fn):
 
+def const_violation(x , Fn):
     D, P = constrain_const(Fn)                                                  # P = number of constraint
-                                                                                # D = Dimension of the function
+    phi_indv = []                                                               # D = Dimension of the function
     for p in np.arange(P):
         ap = 0
         for d in np.arange(D):
             Cp_old = np.mod(D - (p+1) + (d+1) + 1 , D)
             Cp = np.where(Cp_old != 0 , Cp_old , D)
-            ap = sum([(Cp * a[d]) ** 2])
+            ap = ap + (Cp * x[d]) ** 2
+        
         gp = D**2 - ap
-        phi_indv = max(0,gp)
-        const_viol = sum([phi_indv])                                            # constraint violation
+        phi_indv.append(max(0,gp))
+    const_viol = sum(phi_indv)                                               # constraint violation
 
     return const_viol
 
