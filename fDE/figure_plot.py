0014f740ffb4383394357fe044c30b92bff898fe
neelu065 <52276776+neelu065@users.noreply.github.com>
2019-10-27 01:00:52 +0530

    Add files via upload

--- fDE/figure_plot.py
+++ fDE/figure_plot.py
@@ -1,12 +1,22 @@
-
 import matplotlib.pyplot as plt
+import constrain_const
 
-
-def figure_plot(target , popsize ):
+def figure_plot(target, popsize,Fn,c= None):
     A = [target[i][0] for i in range(popsize)]
     B = [target[i][1] for i in range(popsize)]
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.plot(A , B , '*')
-    plt.show(block = False)
+    plt.plot(A, B, '*')
+    if 1 <= Fn <= 9:
+        value = constrain_const(Fn)  # func which decide the Dimension and parameter(p)
+        D = value[0]
+        plt.ylim(-D,D)
+        plt.xlim(-D,D)
+    if Fn == 11 or Fn == 12:
+        plt.ylim(-6,6)
+        plt.ylim(-6,6)
+    if Fn == 19:
+        plt.ylim(-c,c)
+        plt.xlim(-c,c)
+    plt.show(block=False)
     return ()
--- fDE/func.py
+++ fDE/func.py
@@ -1,20 +1,28 @@
 import numpy as np
-def func1(x):
-    # F1 to F9
-    # Sphere function, use any bounds, f(0,...,0)=0
-    return sum([x[i]**2 for i in range(len(x))])
 
-def func2(x):
-    # F10
-    # Sine function
-    return -np.sin( 5 * np.pi * x )**6 + 1
 
-def func3(x):
-    # F11 to F12
-    # Himmelblau function
-    return ( x[0]**2 + x[1] - 11 )**2 + ( x[0] + x[1]**2 - 7 )**2 + 1
+def fobj(x, Fn):
 
-def func4(x):
-    # F13 to F18
-    # Modified version of Rastrigin function
-    return sum([10(1+np.cos(2*np.pi*kd*x[i])) + 2*kd*(x[i] - 1)**2 * H[x[i] -1] for i in range(len(x))])
+    if 1 <= Fn <= 9:
+        # Sphere function, use any bounds, f(0,...,0)=0
+        return np.sum(x ** 2)
+
+    if Fn == 10:
+        # F10
+        # Sine function
+        return -np.sin(5 * np.pi * x) ** 6 + 1
+
+    if Fn == 11 or Fn == 12:
+        # F11 to F12
+        # Himmelblau function
+        return (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2 + 1
+
+# def func4(x):
+#    # F13 to F18
+#    # Modified version of Rastrigin function
+#    return sum([10(1+np.cos(2*np.pi*kd*x[i])) + 2*kd*(x[i] - 1)**2 * H[x[i] -1] for i in range(len(x))])
+#
+    
+    if Fn == 19:
+        # Egg holder function
+        return -(x[1] + 47) * np.sin(np.sqrt(abs(x[1] + 0.5 * x[0] + 47))) - x[0] * np.sin(np.sqrt(abs(x[0] - x[1] - 47)))
