dcc5554f569d8cfc92ec3a565706c606ebe5a48a
neelu065 <52276776+neelu065@users.noreply.github.com>
2019-10-27 22:18:21 +0530

    Delete mutation.py

--- mutation.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def mutation1(popsize , j , pop):
-    idxs = [idx for idx in range(popsize) if idx != j]                  # check to make sure that own index is not selected
-    a, b, c = pop[np.random.choice(idxs, 3, replace=False)]             # random choice among popsize excluding current index
-    mutant = np.clip(a + mut * (b - c), 0, 1)
-    return mutant
